0f49650f1bf513e767cc9e95e81268448fb55024
MohammedHW <hoshsangabadwalam@gmail.com>
2026-04-28 12:34:38 +0530

    otp sender

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test-mail', function () {
+    Mail::to('hoshangabadwalam@gmail.com')->send(new \App\Mail\SendOtpMail('123456'));
+    return 'Mail sent';
+});
