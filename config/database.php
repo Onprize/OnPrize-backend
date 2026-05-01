6078c3a1854dcd092d92057960a7a0cae10e5803
MohammedHW <hoshsangabadwalam@gmail.com>
2026-05-01 13:27:46 +0530

    backened update

--- config/database.php
+++ config/database.php
@@ -56,11 +56,6 @@ return [
     'prefix' => '',
     'strict' => true,
     'engine' => null,
-
-    'options' => extension_loaded('pdo_mysql') ? [
-        \PDO::MYSQL_ATTR_SSL_CA => env('DB_SSL_CA'),
-        \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-    ] : [],
 ],
 
 
